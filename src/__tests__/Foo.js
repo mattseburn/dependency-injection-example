c6db5df02eabf032d76dcda78f5a13c9a4a2729d
Matt Seburn <matt.seburn@telus.com>
2018-03-27 14:50:09 -0400

    correct test name

--- src/__tests__/Foo.js
+++ src/__tests__/Foo.js
@@ -7,7 +7,7 @@ const mockBar = {
 const foo = new Foo(mockBar)
 
 describe('Foo.getBarValue()', () => {
-  it('should return the message from bar', () => {
+  it('should return the value from bar', () => {
     const mockValue = 'Mock instance of Bar'
     mockBar.getValue.mockReturnValue(mockValue)
 
